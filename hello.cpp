d96bd34088e5431e7a4c07d4a61f8840928c1f91
Nishaan Padanthaya <145466368+NishaanPadanthaya@users.noreply.github.com>
2025-03-14 23:19:20 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+  cout << "Now I have entered hello.cpp file successfully\n";
+  cout << "By Nishaan \n";
+  return 0;
+}
